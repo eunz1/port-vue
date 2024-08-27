135b8f304874b50c7e9136b24056369ad13e03be
송은지 <song-eunji@song-eunjiui-MacBookAir.local>
2024-08-27 16:36:09 +0900

    배포3

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import "./assets/SCSS/style.scss";
+import "./assets/scss/style.scss";
 
 const app = createApp(App)
 
